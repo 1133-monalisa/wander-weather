e603534937fa31de48bb12d670e726c48c1e5ca9
1133-monalisa <chandmonalisa9@gmail.com>
2025-11-01 13:52:13 +0545

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen bg-background w-full'>
+      <h1>Wander Weather</h1>
+    </div>
+  )
+}
+
+export default page
